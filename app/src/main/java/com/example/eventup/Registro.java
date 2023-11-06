18c834eb296bfac980b15f5af06bf0a69fd65850
carloosbravo <carloosbravo04@gmail.com>
2023-11-07 00:11:30 +0100

    Regitro creado Main modificado

---
+++ app/src/main/java/com/example/eventup/Registro.java
@@ -0,0 +1,56 @@
+package com.example.eventup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.eventup.dbHelper.DatabaseAux;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+    public void changeToMain(View view) {
+        Intent nIntent = new Intent(Registro.this, MainActivity.class);
+        startActivity(nIntent);
+    }
+
+    public void InsertValues(View v){
+        TextView nameTextView = findViewById(R.id.editTextUserName);
+        TextView emailTextView = findViewById(R.id.editTextEmail);
+
+        String nameString = nameTextView.getText().toString();
+        String emailString = emailTextView.getText().toString();
+
+        DatabaseAux aux = new DatabaseAux(Registro.this);
+        SQLiteDatabase db = aux.getWritableDatabase();
+
+        if(db != null && !nameString.isEmpty() && !emailString.isEmpty()) {
+            ContentValues values = new ContentValues();
+            values.put("name", nameString);
+            values.put("email", emailString);
+
+            long res = db.insert("users", null, values);
+            if(res >= 0) {
+                Toast.makeText(this, "Insertado correctamente", Toast.LENGTH_LONG).show();
+                nameTextView.setText("");
+                emailTextView.setText("");
+            }
+            else {
+                Toast.makeText(this, "Fallo al insertar", Toast.LENGTH_LONG).show();
+            }
+            db.close();
+        }
+
+    }
+}
