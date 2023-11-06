dcbd441502ffc6e1f29db8baa92d9365c68d8fa4
carloosbravo <carloosbravo04@gmail.com>
2023-11-07 00:48:01 +0100

    logo añadido al main

--- app/src/main/java/com/example/eventup/MostrarUsuarios.java
+++ app/src/main/java/com/example/eventup/MostrarUsuarios.java
@@ -2,7 +2,9 @@ package com.example.eventup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MostrarUsuarios extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MostrarUsuarios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_usuarios);
+    }
+
+    public void changeToMain(View view) {
+        Intent nIntent = new Intent(MostrarUsuarios.this, MainActivity.class);
+        startActivity(nIntent);
     }
 }
